3fb12405bae009cfa0d067fd771386bd5598e29b
ccdale <chris.allison@hivehome.com>
2023-03-19 08:37:29 +0000

    add banner and license

--- src/cliptube/ct.py
+++ src/cliptube/ct.py
@@ -16,3 +16,9 @@
 #     You should have received a copy of the GNU General Public License
 #     along with cliptube.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+import sys
+
+import pyclip
+
+from cliptube import __version__, errorExit, errorNotify, errorRaise
