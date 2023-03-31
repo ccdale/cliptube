bcf41bc2c4bbdccb1987784a12f1b8f5ef7caa9e
ccdale <chris.allison@hivehome.com>
2023-03-31 07:58:47 +0100

    Bumping version from 0.1.10 to 0.1.11

--- src/cliptube/__init__.py
+++ src/cliptube/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
 
 def errorNotify(exci, e, fname=None):
--- tests/test_cliptube.py
+++ tests/test_cliptube.py
@@ -34,7 +34,7 @@ class TheException(Exception):
 
 
 def test_cliptube_version():
-    assert __version__ == "0.1.10"
+    assert __version__ == "0.1.11"
 
 
 def test_errorNotify(capsys):
