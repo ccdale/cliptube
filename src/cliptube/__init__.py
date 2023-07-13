21d5ec8eb668580e1ef5e3e9dc9335744a71ea83
ccdale <chris.allison@hivehome.com>
2023-07-13 08:57:57 +0100

    Bumping version from 0.8.2 to 0.8.3

--- src/cliptube/__init__.py
+++ src/cliptube/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 import ccalogging
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 __appname__ = "cliptube"
 
 log = ccalogging.log
--- tests/test_cliptube.py
+++ tests/test_cliptube.py
@@ -34,7 +34,7 @@ class TheException(Exception):
 
 
 def test_cliptube_version():
-    assert __version__ == "0.8.2"
+    assert __version__ == "0.8.3"
 
 
 def test_errorNotify(capsys):
