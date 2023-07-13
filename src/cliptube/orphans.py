9f1603f11a5eeb0bae58feb5d953630f834cdf93
ccdale <chris.allison@hivehome.com>
2023-07-13 08:40:14 +0100

    initial orphaned detection

---
+++ src/cliptube/orphans.py
@@ -0,0 +1,62 @@
+import mimetypes
+import os
+import sys
+
+import ccalogging
+
+from cliptube import __appname__, __version__, errorExit, errorNotify, errorRaise
+from cliptube.config import readConfig
+
+logfile = os.path.abspath(os.path.expanduser(f"~/{__appname__}.log"))
+ccalogging.setLogFile(logfile)
+ccalogging.setDebug()
+# ccalogging.setInfo()
+log = ccalogging.log
+
+
+def getSubsPath(cfg):
+    try:
+        xp = cfg["mediaserver"]["incomingdir"]
+        if xp.startswith("/"):
+            return xp
+        return os.path.abspath(os.path.expanduser(f"~/{xp}"))
+    except Exception as e:
+        errorExit(sys.exc_info()[2], e)
+
+
+def hasVideo(filelist):
+    try:
+        for f in filelist:
+            xtype, xenc = mimetypes.guess_type(f)
+            if xtype.startswith("video"):
+                return True
+        return False
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def findOrphans():
+    try:
+        log.info(f"starting {__appname__} {__version__} findOrphans")
+        cfg = readConfig()
+        path = getSubsPath(cfg)
+        log.debug(f"changing working directory to {path}")
+        os.chdir(path)
+        files = os.listdir()
+        log.debug(f"found {len(files)} files in {path}")
+        entries = {}
+        deletelist = []
+        for file in files:
+            fn, ext = os.path.splitext(file)
+            _ = entries.get(fn, [])
+            entries[fn].append(file)
+        log.debug(f"found {len(entries.keys())} file sets in {path}")
+        for fn in entries:
+            log.debug(f"checking whether {fn} has orphaned subtitle files")
+            if not hasVideo(entries[fn]):
+                log.debug(f"{fn} is orphaned")
+                deletelist.extend(entries[fn])
+        log.info(f"found {len(deletelist)} files to delete")
+        log.debug(f"will delete {deletelist}")
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
--- src/cliptube/watchdir.py
+++ src/cliptube/watchdir.py
@@ -1,17 +1,17 @@
 import os
 from signal import signal, SIGINT
 import sys
-from threading import Event, Thread
+from threading import Event
 import time
 
 import ccalogging
-import PySimpleGUIQt as sg
 
 from cliptube import __appname__, __version__, errorExit, errorNotify, errorRaise
 from cliptube.config import readConfig
 from cliptube.shell import shellCommand
 
-ccalogging.setLogFile(f"/home/chris/log/{__appname__}.log")
+logfile = os.path.abspath(os.path.expanduser(f"~/{__appname__}.log"))
+ccalogging.setLogFile(logfile)
 # ccalogging.setDebug()
 ccalogging.setInfo()
 log = ccalogging.log
@@ -78,45 +78,23 @@ def watchDir(path, sleeptime=60):
         errorNotify(sys.exc_info()[2], e)
 
 
+def getWatchPath(cfg):
+    try:
+        xp = cfg["youtube"]["incomingdir"]
+        if xp.startswith("/"):
+            return xp
+        return os.path.abspath(os.path.expanduser(f"~/{xp}"))
+    except Exception as e:
+        errorExit(sys.exc_info()[2], e)
+
+
 def dirWatch():
     try:
         log.info(f"starting {__appname__} {__version__} dirWatch")
         cfg = readConfig()
-        path = os.path.abspath(os.path.expanduser(f"~/{cfg['youtube']['incomingdir']}"))
+        path = getWatchPath(cfg)
         log.info(f"dirWatch will watch {path}")
         watchDir(path)
         log.info("dirWatch completed")
     except Exception as e:
-        errorNotify(sys.exc_info()[2], e)
-
-
-def doTray():
-    try:
-        global ev
-        # wayland check for QT
-        xserver = os.environ.get("XDG_SESSION_TYPE", "Xorg")
-        if xserver == "wayland":
-            os.environ["QT_QPA_PLATFORM"] = "wayland"
-        fred = Thread(target=dirWatch, args=[])
-        fred.start()
-        log.info(f"Starting tray icon for {__appname__} mediaserver directory watcher")
-        menudef = ["BLANK", ["E&xit"]]
-        iconfn = os.path.abspath(
-            os.path.expanduser(rf"~/.local/share/image/dirwatch-green.png")
-        )
-        tray = sg.SystemTray(
-            menu=menudef,
-            filename=iconfn,
-            tooltip=f"{__appname__} directory watcher",
-        )
-        while True:
-            menuitem = tray.read()
-            if menuitem == "Exit":
-                ev.set()
-                break
-        log.info("removing tray icon for directory watcher")
-        log.info("waiting for thread to end")
-        fred.join()
-        log.info("thread has ended")
-    except Exception as e:
-        errorNotify(sys.exc_info()[2], e)
+        errorExit(sys.exc_info()[2], e)
